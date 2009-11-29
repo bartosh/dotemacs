5a73247d8557be0a7ecfd0c967966a28f0b379c8
Ed Bartosh <bartosh@gmail.com>
2009-11-29 23:31:51 +0200

    assigned global keys

--- keys.el
+++ keys.el
@@ -4,3 +4,17 @@
     (beginning-of-line)
     (kill-line))
 (global-set-key "\C-y" 'Kill-Line)
+
+(global-set-key [(alt z)] 'undo)
+(global-set-key "\M-g" 'goto-line)
+
+;; examples
+;;(global-set-key [(ctrl f2)] 'save-buffer)
+;;(global-set-key [(s f2)] 'dired) ;; windows key + F2
+;;(global-set-key [(shift f2)] 'ibuffer)
+
+;; Unassigned simple key combinations
+;; [(ctrl f1)] - [(ctrl f12)]
+;; [(shift f1)] - [(shift f12)]
+;; [(s f1)] - [(s f12)] - windows key + F1-F12
+
