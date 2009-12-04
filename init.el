bd3b22e944cd73776732b0295a2e1a1523d188cc
Ed Bartosh <eduard.bartosh@nokia.com>
2009-12-04 11:54:55 +0200

    flymake + pylint again

--- init.el
+++ init.el
@@ -55,6 +55,19 @@
 ;; python settings
 (require 'python-mode)
 
+;; check python code with flymake+pylint
+(when (load "flymake" t)
+  (defun flymake-pylint-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "epylint" (list local-file))))
+
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pylint-init)))
+
 (defun my-python-hook ()
   (require 'whitespace)
   (whitespace-mode t)
@@ -70,6 +83,7 @@
   ;;(linum-mode t)
   (transient-mark-mode t)
   (require 'pycomplete)
+  (flymake-mode)
 
   ;; rope
   (require 'pymacs)
