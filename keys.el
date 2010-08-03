a36edfc71abf9124b075ad661cdb83041c5b5e4d
Ed Bartosh <eduard.bartosh@nokia.com>
2010-08-03 14:06:38 +0300

    Added some windows-like region-related keystrokes (just for reminding, commented)

--- keys.el
+++ keys.el
@@ -14,10 +14,13 @@
 
 (global-set-key [(shift f2)] 'ibuffer)
 
-
-
 ;; Unassigned simple key combinations
 ;; [(ctrl f1)] - [(ctrl f12)]
 ;; [(shift f1)] - [(shift f12)]
 ;; [(s f1)] - [(s f12)] - windows key + F1-F12
 
+;;(global-set-key [(shift delete)] 'kill-region)
+;;(global-set-key [(ctrl insert)] 'copy-region-as-kill)
+;;(global-set-key [(shift insert)] 'yank)
+;;(global-set-key [(ctrl delete)] 'Kill-Line)
+ 
